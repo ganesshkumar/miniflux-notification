96d05b3b392e3bc41878a0c6fb32c2f825c58197
Ganessh Kumar R P <garp@microsoft.com>
2018-10-19 00:47:28 +0530

    fix variable name collision

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import requests
 import os
 
 notify_url = os.environ['REACT_NOTIFY_URL_WITH_CODE']
-miniflux = os.environ['MINIFLUX']
+miniflux_site = os.environ['MINIFLUX']
 username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
-client = miniflux.Client(miniflux, username, password)
+client = miniflux.Client(miniflux_site, username, password)
 
 # Unread entries notification
 entries = client.get_entries(status="unread", limit=1000, direction="desc")
@@ -15,9 +15,9 @@ message = ''
 if 'total' in entries:
     unread_count = entries['total']
     if unread_count > 0 and unread_count < 1000:
-        message = f'You have {unread_count} unread entries at {miniflux}'
+        message = f'You have {unread_count} unread entries at {miniflux_site}'
     elif unread_count is 1000:
-        message = f'You have 1000+ unread entries at {miniflux}'
+        message = f'You have 1000+ unread entries at {miniflux_site}'
 
 print(message)
 
